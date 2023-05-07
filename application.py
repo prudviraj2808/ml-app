ee65d4ffa52d5df1b5fb288765eda435ec375ac3
prudhvi raj <prudvirajeluri@gmail.com>
2023-05-08 00:30:16 +0530

    Restored to original format

--- application.py
+++ application.py
@@ -38,9 +38,8 @@ def predict_datapoint():
         print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
-        prdicted_results=results[0]
-        return 'The predicted score is: {}'.format(prdicted_results)
-        # return render_template('home.html', results='The predicted score is: {}'.format(prdicted_results)) # rendering the predicted result
+        prdicted_results=str(results[0])
+        return render_template('home.html', results=prdicted_results) # rendering the predicted result
 
     
     
