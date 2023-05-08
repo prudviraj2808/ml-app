368012dbd533a8e3f09b1ab64265245e0c86fdb5
prudhvi raj <prudvirajeluri@gmail.com>
2023-05-08 12:18:01 +0530

    Updated the action in home.html

--- application.py
+++ application.py
@@ -36,8 +36,8 @@ def predict_datapoint():
         print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
-        prdicted_results=str(results[0])
-        return render_template('home.html',results=prdicted_results) # rendering the predicted result
+        prdicted_results=results[0]
+        return render_template('home.html',results=prdicted_results.format()) # rendering the predicted result
 
         
 if __name__=="__main__":
