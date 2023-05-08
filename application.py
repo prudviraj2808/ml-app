ea319a1f884ac15185e7a5ec5f3cfd8e4e0fc35b
prudhvi raj <prudvirajeluri@gmail.com>
2023-05-09 00:28:31 +0530

    Updated sk learn package version

--- application.py
+++ application.py
@@ -19,27 +19,27 @@ def index():
 def predict_datapoint():
     if request.method=='POST':
         data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))
+            gender=request.form['gender'],
+            race_ethnicity=request.form['ethnicity'],
+            parental_level_of_education=request.form['parental_level_of_education'],
+            lunch=request.form['lunch'],
+            test_preparation_course=request.form['test_preparation_course'],
+            reading_score=float(request.form['reading_score']),
+            writing_score=float(request.form['writing_score'])
 
         )
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
         print("Before Prediction")
-
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
+        print('After Prediction')
         prdicted_results=results[0]
         return render_template('home.html', results='The predicted score is : {}'.format(prdicted_results))
         # return render_template('home.html',results=prdicted_results) # rendering the predicted result
 
         
 if __name__=="__main__":
-    app.run()        
+    app.run(host='0.0.0.0')        
 
 
